9e6448860e6a5952d334d9715a5d229a57422ff7
Gatis <gatis.silins92@gmail.com>
2021-12-01 01:04:50 +0200

    Added local storage

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import './App.scss';
 import Input from './components/input';
 import List from './components/list';
 import Progress from './components/progress';
+import useLocalStorage from './hooks/localStorage';
+import { TODO } from './components/item';
 
 const App = () => {
-  const [todos, setTodos] = useState([{
+  const [todos, setTodos] = useLocalStorage('todos', [{
     id: 1, name: 'Wash dishes', priority: 1, done: false,
   }, {
     id: 2, name: 'Take out the trash.', priority: 0, done: false,
@@ -13,7 +15,7 @@ const App = () => {
   {
     id: 3, name: 'Take out the dog.', priority: 2, done: false,
   }]);
-  const progress = ((todos.filter((item) => item.done).length / todos.length) * 100).toFixed(0);
+  const progress = ((todos.filter((item: TODO) => item.done).length / todos.length) * 100).toFixed(0);
 
   return (
     <div className="App">
